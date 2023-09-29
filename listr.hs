2f07ce8100a769d1ce460dcc0e41705fced99efb
noco <84770894+bx4ay@users.noreply.github.com>
2023-09-29 13:28:43 +0900

    仕様の変更

--- listr.hs
+++ listr.hs
@@ -1,40 +1,52 @@
 {-# LANGUAGE LambdaCase #-}
+
 import Data.Bifunctor (first)
 import System.Environment (getArgs)
+
 newtype Term = T [Term]
+
+unT :: Term -> [Term]
 unT (T x) = x
-exec = exec' . tail . scanl (\ cases
+
+exec :: [Char] -> Term -> Term
+exec = exec1 . tail . scanl (\ cases
     ('(', i) c -> (c, i + 1)
     (')', i) c -> (c, i - 1)
-    ('[', i) c -> (c, i + 1)
-    (']', i) c -> (c, i - 1)
     (_, i) c -> (c, i)
-    ) (' ', 0) . filter (`elem` "().01[]|") where
-    exec' = \ case
+    ) (' ', 0) . filter (`elem` "().01|") where
+    exec1 = \ case
         [] -> id
-        t -> (\ case
-            (t0, _ : t1) -> \ x -> T $ exec'' t0 x : unT (exec' t1 x)
-            _ -> exec'' t
-            ) $ span (/= ('.', snd $ head t)) t
-    exec'' = \ case
+        s -> (\ case
+            (s0, _ : s1) -> head . dropWhile (not . null . unT . exec2 s0) . iterate (exec1 s1)
+            _ -> exec2 s
+            ) $ span (/= ('|', snd $ head s)) s
+    exec2 = \ case
         [] -> id
-        ('0', _) : t -> \ case
-            T (x0 : _) -> exec' t x0
+        s -> (\ case
+            (s0, _ : s1) -> \ x -> T $ exec3 s0 x : unT (exec2 s1 x)
+            _ -> exec3 s
+            ) $ span (/= ('.', snd $ head s)) s
+    exec3 = \ case
+        [] -> id
+        ('0', _) : s -> \ case
+            T (x : _) -> exec3 s x
             x -> x
-        ('1', _) : t -> \ case
-            T (_ : x1) -> exec' t $ T x1
+        ('1', _) : s -> \ case
+            T (_ : x) -> exec3 s $ T x
             x -> x
-        ('(', i) : t -> (\ case
-            (t0, _ : t1) -> exec'' t1 . exec' t0
+        ('(', i) : s -> (\ case
+            (s0, _ : s1) -> exec3 s1 . exec1 s0
             _ -> errorWithoutStackTrace "parse error"
-            ) $ span (/= (')', i + 1)) t
-        ('[', i) : t -> (\ case
-            ((t0, _ : t1), _ : t2) -> exec'' t2 . head . dropWhile (not . null . unT . exec' t0) . iterate (exec' t1)
-            _ -> errorWithoutStackTrace "parse error"
-            ) $ first (span (/= ('|', i + 1))) $ span (/= (']', i + 1)) t
+            ) $ span (/= (')', i + 1)) s
         _ -> errorWithoutStackTrace "parse error"
+
+fromStr :: [Char] -> Term
 fromStr = T . map (T . (`replicate` T []) . fromEnum)
+
+toStr :: Term -> [Char]
 toStr = map (toEnum . length . unT) . unT
+
+main :: IO ()
 main = do
     args <- getArgs
     [code, input] <- case args of
