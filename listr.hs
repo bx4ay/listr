72b9b291500c5431c9b6089ca117c53f671e0f0d
noco <84770894+bx4ay@users.noreply.github.com>
2024-02-11 20:21:38 +0900

    Update listr.hs

--- listr.hs
+++ listr.hs
@@ -16,12 +16,12 @@ exec = exec1 . drop 1 . scanl (\ cases
 
     exec1 :: [(Char, Int)] -> Term -> Term
     exec1 = \ case
+        [] -> id
         s@((_, i) : _) -> (\ case
             (s0, ('.', _) : s1) -> \ x -> T $ exec2 s0 x : unT (exec1 s1 x)
             (s0, ('|', _) : s1) -> until (null . unT . exec2 s0) $ exec1 s1
             _ -> exec2 s
-            ) $ break (`elem` (zip ".|" [i, i])) s
-        _ -> id
+            ) $ break (`elem` zip ".|" [i, i]) s
 
     exec2 :: [(Char, Int)] -> Term -> Term
     exec2 = \ case
