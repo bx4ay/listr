98c8cd4cdb9bec03fd0939b27473928dcb605430
noco <84770894+bx4ay@users.noreply.github.com>
2026-06-21 12:02:37 +0900

    Update listr.hs

--- listr.hs
+++ listr.hs
@@ -8,23 +8,23 @@ readL = List . map (List . (`replicate` List []) . fromEnum)
 showL :: List -> String
 showL = map (toEnum . length . list) . list
 
-data Term = Head [Term] | Tail [Term] | Cons [Term] [Term] | While [Term] [Term]
+data Term = Head | Tail | Cons [Term] [Term] | While [Term] [Term]
 
 parse :: String -> [Term]
 parse s = case parse1 $ concatMap (takeWhile (/= '#')) $ lines s of
-    (x, "") -> x
+    (x, []) -> x
     _ -> errorWithoutStackTrace "unmatched brackets"
     where
     parse1 :: String -> ([Term], String)
     parse1 s = let (x, s1) = parse2 [] s in case s1 of
-        '.' : s2 -> let (y, s2) = parse1 s2 in ([Cons x y], s2)
-        '|' : s2 -> let (y, s2) = parse1 s2 in ([While x y], s2)
+        '.' : s2 -> let (y, s3) = parse1 s2 in ([Cons x y], s3)
+        '|' : s2 -> let (y, s3) = parse1 s2 in ([While x y], s3)
         _ -> (x, s1)
 
     parse2 :: [Term] -> String -> ([Term], String)
     parse2 x [] = (x, [])
-    parse2 x ('0' : s) = parse2 (x ++ [Head []]) s
-    parse2 x ('1' : s) = parse2 (x ++ [Tail []]) s
+    parse2 x ('0' : s) = parse2 (x ++ [Head]) s
+    parse2 x ('1' : s) = parse2 (x ++ [Tail]) s
     parse2 x ('(' : s) = case parse1 s of
         (y, ')' : s1) -> parse2 (x ++ y) s1
         _ -> errorWithoutStackTrace "unmatched brackets"
@@ -35,8 +35,8 @@ exec :: [Term] -> List -> List
 exec x l = foldl exec1 l x
     where
     exec1 :: List -> Term -> List
-    exec1 l (Head x) = head $ (++ [List []]) $ list $ exec x l
-    exec1 l (Tail x) = List $ drop 1 $ list $ exec x l
+    exec1 l Head = head $ list l ++ [List []]
+    exec1 l Tail = List $ drop 1 $ list l
     exec1 l (Cons x y) = List (exec x l : list (exec y l))
     exec1 l (While x y) = until (null . list . exec x) (exec y) l
 
